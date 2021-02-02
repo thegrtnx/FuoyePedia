324cfc8759501b83cacface369606881a4cee7a7
Grtnxhor <Greatnessabolade@gmail.com>
2021-02-02 19:04:24 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# FuoyePedia
+ An online PDF bank for Fuoye
+
+visit https://fuoyepedia.com.ng for details
---
+++ service-worker.js
@@ -0,0 +1,37 @@
+var CACHE_NAME = 'pwgen-cache';
+var urlsToCache = [
+    'index.html'
+];
+
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "offline.html";
+
+console.log('loading sw');
+
+self.addEventListener('install', function(event) {
+    // Perform install steps
+    console.log('installing sw');
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                console.log('Opened cache');
+                var x = cache.addAll(urlsToCache);
+                console.log('cache added');
+                return x;
+            })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                    // Cache hit - return response
+                    if (response) {
+                        return response;
+                    }
+                    return fetch(event.request);
+                }
+            )
+    );
+});
